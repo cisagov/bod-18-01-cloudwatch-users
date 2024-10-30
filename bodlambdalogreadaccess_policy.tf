82312c9a7dae57db03e14caf4b5ac7af34252780
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-10-30 13:46:29 -0400

    Allow filtering on log streams
    User @jeffkause is unable to filter on log events in the
    /aws/lambda/task_pshtt log group without this change.
    
    Resolves #26.

--- bodlambdalogreadaccess_policy.tf
+++ bodlambdalogreadaccess_policy.tf
@@ -21,7 +21,7 @@ data "aws_iam_policy_document" "bodlambdalogreadaccess_policy_doc" {
       "logs:GetLogEvents",
     ]
     effect    = "Allow"
-    resources = [for k, v in data.aws_cloudwatch_log_group.bod_lambda_logs : v.arn]
+    resources = flatten([for k, v in data.aws_cloudwatch_log_group.bod_lambda_logs : [v.arn, "${v.arn}:log-stream:"]])
   }
 }
 
